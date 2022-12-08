300f0d98c64a18a9c5e2aec507af6875d5b27d05
sunjing <sunjing@mzhcorp.com>
2022-12-09 01:01:36 +0800

    feat: life cycle

--- pages/index/index.js
+++ pages/index/index.js
@@ -17,7 +17,8 @@ Page({
   },
 
   toLogs() {
-    wx.navigateTo({
+    // wx.navigateTo({ // 调用 onHide
+    wx.reLaunch({ // 调用 onUnload
       url: '/pages/image/images',
     })
   },
@@ -40,48 +41,49 @@ Page({
    * Lifecycle function--Called when page is initially rendered
    */
   onReady() {
-
+    console.log("onReady")
   },
 
   /**
    * Lifecycle function--Called when page show
    */
   onShow() {
-
+    console.log("onShow")
   },
 
   /**
    * Lifecycle function--Called when page hide
    */
   onHide() {
-
+    console.log("onHide")
   },
 
   /**
    * Lifecycle function--Called when page unload
    */
   onUnload() {
-
+    // 页面销毁
+    console.log("page unload")
   },
 
   /**
    * Page event handler function--Called when user drop down
    */
   onPullDownRefresh() {
-
+    console.log("onPullDownRefresh")
   },
 
   /**
    * Called when page reach bottom
    */
   onReachBottom() {
-
+    console.log("onReachBottom")
   },
 
   /**
    * Called when user click on the top right corner to share
    */
   onShareAppMessage() {
-
+    console.log("onShareAppMessage")
   }
 })
